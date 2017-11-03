e9ffad301867c8a94e9e6e49e7306df8cc6a164c
Florian Link <burg.fl@gmail.com>
2017-11-03 08:55:58 +0100

    Polish relution upload script

--- relution-upload/relution_upload_app.sh
+++ relution-upload/relution_upload_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if [ ! $JQ_EXECUTABLE ]; then 
-    JQ_EXECUTABLE="jq"
+if [[ -z "$JQ_EXECUTABLE" ]]; then
+	JQ_EXECUTABLE="jq"
 fi
 
 while [[ $# > 1 ]]
@@ -60,38 +60,38 @@ if [ $RU_HELP ] ; then
     echo ""
     echo "-f --file               Relative path of the artifact that you want to deploy to the Relution Enterprise App Store, relative to the workspace directory. This is typically an Apple iOS (.ipa) or Google Android (.apk) binary."
     echo "-h --host               The Relution base url to which the file should be deployed."
-    echo "-r --release_status     The release status in which the file should be put."  
+    echo "-r --release_status     The Release status in which the file should be put."
     echo "-e --environment        The development hub environment id."
-    echo "-a --api_key          Relution API Token used for the authentication."    
+    echo "-a --api_key          Relution API Token used for the authentication."
 fi
 
-if [ $RU_RELEASE_STATUS ]; then 
+if [[ -n "$RU_RELEASE_STATUS" ]]; then
     curl_args="?releaseStatus=$RU_RELEASE_STATUS"
 else
     curl_args="?releaseStatus=DEVELOPMENT"
 fi
 
-if [ $RU_ENVIRONMENT_UUID ]; then 
-    curl_args="${curl_args}&environmentUuid=$RU_ENVIRONMENT_UUID" 
+if [[ -n "$RU_ENVIRONMENT_UUID" ]]; then
+    curl_args="${curl_args}&environmentUuid=$RU_ENVIRONMENT_UUID"
 fi
 
-if [ ! $RU_FILE ]; then 
+if [[ -z "$RU_FILE" ]]; then
     echo "Use -f to pass the file path"
     exit 1
 fi
 
-if [ $RU_API_KEY ]; then
+if [[ -n "$RU_API_KEY" ]]; then
     curl_auth="-H X-User-Access-Token:${RU_API_KEY}"
 else
     curl_auth="-u ${RU_USER}:${RU_PASSWORD}"
 fi
 
 echo "Uploading '$RU_FILE' to '$RU_HOST/relution/api/v1/apps$curl_args' ..."
-response=$(curl $curl_auth -F app=@$PWD/$RU_FILE $RU_HOST/relution/api/v1/apps$curl_args)
-echo $response | $JQ_EXECUTABLE '.message'
-response_code=$(echo $response | $JQ_EXECUTABLE -r '.status')
-if [[ $response_code == "0" ]]; then
-    appuuid=$(echo $response | $JQ_EXECUTABLE -r '.results[0].uuid')
+response=$(curl "$curl_auth" -F "app=@$PWD/$RU_FILE" "$RU_HOST/relution/api/v1/apps$curl_args")
+echo "$response" | "$JQ_EXECUTABLE" '.message'
+response_code=$(echo "$response" | "$JQ_EXECUTABLE" -r '.status')
+if [[ "$response_code" == "0" ]]; then
+    appuuid=$(echo "$response" | "$JQ_EXECUTABLE" -r '.results[0].uuid')
     echo "$RU_HOST/relution/portal/#/apps/$appuuid/information"
 else
     echo "Upload failed!"
